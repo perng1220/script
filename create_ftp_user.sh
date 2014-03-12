f6723c6c3a665f51db72ee3f888527a88c7d9d3b
perng <perngs@hotmail.com>
2014-03-12 19:52:44 +0800

    modify create_ftp_user.sh

--- create_ftp_user.sh
+++ create_ftp_user.sh
@@ -4,52 +4,51 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin
 create_customer ()
 {
     read -p "Input customer's name: " customer
-    read -s -p "Input customer's password: " c_password
+    if [ -z "$customer" ] || id $customer > /dev/null 2>&1; then
+        echo "Error: Customer's name is empty or exist"
+        exit 6
+    fi
+    read -s -p "Input customer's password: " password
     echo ""
     read -s -p "Confirm password: " confirm_pwd
-    if [ "$c_password" != "$confirm_pwd" ]; then
+    if [ -z "$password" ] || [ "$password" != "$confirm_pwd" ]; then
         echo ""
-        echo "Password does not match the confirm password."
-        exit 6
+        echo "Password does not match the confirm password or is empty."
+        exit 4
     fi
     echo ""
     read -p "Project owner: " owner
-    if id $customer > /dev/null 2>&1; then
-        echo "Error: $owner is not exist"
-        exit 4
-    fi
-    if [ -n "$customer" ] && [ -n "$c_password" ] && ! id $customer > /dev/null 2>&1; then
-        useradd -m -d /var/ftp/$owner/$customer $customer
-        usermod -a -G $customer $owner
-        usermod -a -G $customer chief
-        echo $c_password | passwd --stdin $customer
-        chmod 2770 /var/ftp/$owner/$customer
-    else
-        echo "Error: Customer's name is empty or exist"
+    if [ -z "$owner" ] || ! id $owner > /dev/null 2>&1; then
+        echo "Error: $owner is not exist or empty"
         exit 2
     fi
+    useradd -m -d /var/ftp/$owner/$customer -s /sbin/nologin $customer
+    usermod -a -G $customer $owner
+    usermod -a -G $customer chief
+    echo $password | passwd --stdin $customer
+    chmod 2770 /var/ftp/$owner/$customer
 }
 
 create_staff ()
 {
     read -p "Input staff's name: " staff
-    read -s -p "Input staff's password: " s_password
-    echo ""
-    read -s -p "Confirm password: " confirm_pwd
-    if [ "$s_password" != "$confirm_pwd" ]; then
-        echo ""
-        echo "Password does not match the confirm password."
-        exit 5
-    fi
-    if [ -n "$staff" ] && [ -n "$s_password" ] && ! id $staff > /dev/null 2>&1; then
-        useradd -m -d /var/ftp/$staff $staff
-        usermod -a -G $staff chief
-        chmod 2775 /var/ftp/$staff
-        echo $s_password | passwd --stdin $staff
-    else
+    if [ -z "$staff" ] || id $staff > /dev/null 2>&1; then
         echo "Error: Staff's name is empty or exist"
         exit 3
     fi
+    read -s -p "Input staff's password: " password
+    echo ""
+    read -s -p "Confirm password: " confirm_pwd
+    echo ""
+    if [ -z "$password" ] || [ "$password" != "$confirm_pwd" ]; then
+        echo ""
+        echo "Password does not match the confirm password or is empty."
+        exit 1
+    fi
+    useradd -m -d /var/ftp/$staff $staff
+    usermod -a -G $staff chief
+    chmod 2775 /var/ftp/$staff
+    echo $password | passwd --stdin $staff
 }
 
 echo "1.Create customer's account"
